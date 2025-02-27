5d35f41af777f9103d00c91a28592ff5df183a03
Nadim Taleb <40148047+Xaxabor@users.noreply.github.com>
2025-02-27 15:58:34 +0600

    Update 27. Remove Element.py

--- Other/27. Remove Element.py
+++ Other/27. Remove Element.py
@@ -1,1 +1,19 @@
+class Solution:
+    def removeElement(self, nums: List[int], val: int) -> int:
+        # looking for first val match
+        count = 0
+        valIndex = 0
+        for i in range(len(nums)):
+            if nums[i] == val:
+                valIndex = i
+                break;
+            else:
+                count = count + 1
+        # 
+        for j in range(valIndex+1, len(nums)):
+            if nums[j]!=val:
+                nums[valIndex], nums[j] = nums[j], nums[valIndex]
+                valIndex=valIndex+1
+                count = count + 1
 
+        return count
